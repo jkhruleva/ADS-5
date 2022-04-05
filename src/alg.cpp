6bdb8f984a60ff523fa7337388735069f1b6836c
jkhruleva <99347813+jkhruleva@users.noreply.github.com>
2022-04-05 12:36:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,113 @@
 // Copyright 2021 NNTU-CS
 #include <string>
 #include <map>
+#include <stack>
 #include "tstack.h"
-
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int prior(char z) {
+  switch (z) {
+    case '(':
+      return 0;
+    case ')':
+      return 1;
+    case '+':
+      return 2;
+    case '-':
+      return 2;
+    case '*':
+      return 3;
+    case '/':
+      return 3;
+  }
+  return 0;
+}
+bool search(std::string s, char z) {
+  for (int i = 0; i < s.length(); ++i) {
+    if (s[i] == z) {
+      return true;
+    }
+  }
+  return false;
 }
 
+std::string infx2pstfx(std::string inf) {
+  TStack<char, 100> stack;
+  std::stack<char> Stack;
+  std::string number = "0123456789";
+  std::string operation = "()+-*/";
+  std::string rt = "";
+  for (int i = 0; i < inf.length(); ++i) {
+    if (search(number, inf[i])) {
+      rt += inf[i];
+      rt += ' ';
+    } else if (search(operation, inf[i])) {
+      if (prior(inf[i]) == 0) {
+        Stack.push(inf[i]);
+      } else if (Stack.empty()) {
+        Stack.push(inf[i]);
+      } else if (prior(inf[i]) > prior(Stack.top())) {
+        Stack.push(inf[i]);
+      } else if (inf[i] == ')') {
+        while (Stack.top() != '(') {
+          rt += Stack.top();
+          rt += ' ';
+          Stack.pop();
+        }
+        if (Stack.top() == '(') {
+          Stack.pop();
+        }
+      } else {
+        for (int j = 0; j<= Stack.size(); ++j) {
+          if (prior(Stack.top()) >= prior(inf[i])) {
+            rt += Stack.top();
+            rt += ' ';
+            Stack.pop();
+          }
+        }
+        Stack.push(inf[i]);
+      }
+    }
+  }
+  if (!Stack.empty()) {
+    for (int j = 0; j <= Stack.size(); ++j) {
+      rt += Stack.top();
+      rt += ' ';
+      Stack.pop();
+    }
+  }
+  if (rt[rt.length() - 1] == ' ') {
+    rt.erase(rt.length() - 1);
+  }
+  return rt;
+}
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  int mm = 0;
+  std::stack<int> Stack;
+  TStack<int, 100> stack;
+  std::string number = "0123456789";
+  std::string operation = "()+-*/";
+  for (int i = 0; i < pref.length(); ++i) {
+    if (search(number, pref[i])) {
+      Stack.push(pref[i] - '0');
+    } else if (search(operation, pref[i])) {
+      int a = 0, b = 0;
+      a = Stack.top();
+      Stack.pop();
+      b = Stack.top();
+      Stack.pop();
+      if (pref[i] == '+') {
+        Stack.push(b + a);
+      }
+      if (pref[i] == '-') {
+        Stack.push(b - a);
+      }
+      if (pref[i] == '*') {
+        Stack.push(b * a);
+      }
+      if (pref[i] == '/') {
+        Stack.push(b / a);
+      }
+    }
+  }
+  mm = Stack.top();
+  return mm;
 }
